6132f289efe0077a099dab4bf438328bb8cebf7d
wzyUnicorn <wwlasyh@icloud.com>
2024-08-30 11:01:42 +0800

    ic

---
+++ luke_verify/lab0/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1702977988 /home/ICer/luke_verify/lab0/arbiter.v
+1702977988 /home/ICer/luke_verify/lab0/mcdt.v
+1702977988 /home/ICer/luke_verify/lab0/slave_fifo.v
+1702977988 /home/ICer/luke_verify/lab0/tb1.sv
---
+++ luke_verify/lab1/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,1 @@
+1702977988 /home/ICer/luke_verify/lab1/tb1.v
